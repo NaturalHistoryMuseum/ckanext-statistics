712b285b7aff51e8edfe6f1caa7b7a30dd535f3d
Alice Butcher <alycejenni@gmail.com>
2018-01-18 15:01:53 +0000

    tidy of lib.py and plugin.py
    removed unnecessary imports, reformatting

--- ckanext/statistics/lib.py
+++ ckanext/statistics/lib.py
@@ -1,16 +1,5 @@
-
-#!/usr/bin/env python
+# !/usr/bin/env python
 # encoding: utf-8
 #
 # This file is part of ckanext-statistics
 # Created by the Natural History Museum in London, UK
-
-from pylons import config
-import ckan.model as model
-from ckanext.statistics.model.gbif_download import GBIFDownload
-from ckanext.ckanpackager.model.stat import CKANPackagerStat
-from sqlalchemy import sql, case
-from collections import OrderedDict
-
-
-
--- ckanext/statistics/plugin.py
+++ ckanext/statistics/plugin.py
@@ -4,22 +4,21 @@
 # This file is part of ckanext-statistics
 # Created by the Natural History Museum in London, UK
 
-from logging import getLogger
-import ckan.plugins as p
-from ckanext.statistics.logic.action import download_statistics, dataset_statistics
+from ckanext.statistics.logic.action import dataset_statistics, download_statistics
+
+from ckan.plugins import SingletonPlugin, implements, interfaces
 
 
-class StatisticsPlugin(p.SingletonPlugin):
-    '''
-    NHM Statistics
-    '''
+class StatisticsPlugin(SingletonPlugin):
+    '''NHM Statistics'''
 
-    p.implements(p.IActions)
+    implements(interfaces.IActions)
 
     # IActions
     @staticmethod
     def get_actions():
+        ''' '''
         return {
             u'download_statistics': download_statistics,
             u'dataset_statistics': dataset_statistics
-        }
+            }
