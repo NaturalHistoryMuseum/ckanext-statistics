9d6cc032a4bde8413fcee2ca6f069cfde4c4b70a
Josh Humphries <j.humphries@nhm.ac.uk>
2020-01-13 15:44:04 +0000

    Remove random main function

--- ckanext/statistics/commands/__init__.py
+++ ckanext/statistics/commands/__init__.py
@@ -1,13 +1,5 @@
-# !/usr/bin/env python
+#!/usr/bin/env python
 # encoding: utf-8
 #
 # This file is part of ckanext-statistics
 # Created by the Natural History Museum in London, UK
-
-
-def main():
-    pass
-
-
-if __name__ == u'__main__':
-    main()
