67ba86942a88cadf009b2dae27313b34f09eb3e8
ginger <alycejenni@gmail.com>
2022-05-23 14:30:47 +0100

    Update setup.py
    Update vds version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 
 from setuptools import find_packages, setup
 
-__version__ = '2.0.8'
+__version__ = '2.0.9'
 
 with open('README.md', 'r') as f:
     __long_description__ = f.read()
@@ -17,7 +17,7 @@ def nhm_github(name, tag):
 
 dependencies = dict([
     nhm_github('ckanext-ckanpackager', 'v2.1.1'),
-    nhm_github('ckanext-versioned-datastore', 'v3.4.0'),
+    nhm_github('ckanext-versioned-datastore', 'v3.5.0'),
 ])
 
 setup(
